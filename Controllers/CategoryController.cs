3d9b1111c49288a3c5c21d3d225a38e203dd6ed6
Pedro Evandro Martins <pedroevandromartins@gmail.com>
2025-02-03 16:32:36 -0300

    feat: edit and delete CRUD operations implemented on category

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -32,8 +32,76 @@ public class CategoryController : Controller
         {
             _context.Categories.Add(category);
             _context.SaveChanges();
+            TempData["success"] = "Category created successfully";
             return RedirectToAction("Index");
         }
+        
+        TempData["error"] = "Category creation failed";
         return View();
+    }
+
+    public IActionResult Edit(Guid? id)
+    {
+        if (id is null)
+        {
+            return NotFound();
+        }
+        
+        Category? category = _context.Categories.FirstOrDefault(category => category.Id == id);
+        
+        if (category == null)
+        {
+            return NotFound();
+        }
+        
+        return View(category);
+    }
+
+    [HttpPost]
+    public IActionResult Edit(Category categoryViewModel)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Update(categoryViewModel);
+            _context.SaveChanges();
+            TempData["success"] = "Category updated successfully";
+            return RedirectToAction("Index");
+        }
+        else
+        {
+            return View();
+        }
+    }
+    
+    public IActionResult Delete(Guid? id)
+    {
+        if (id is null)
+        {
+            return NotFound();
+        }
+        
+        Category? category = _context.Categories.FirstOrDefault(category => category.Id == id);
+        
+        if (category == null)
+        {
+            return NotFound();
+        }
+        
+        return View(category);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public IActionResult DeletePOST(Guid? id)
+    {
+        Category? category = _context.Categories.FirstOrDefault(category => category.Id == id);
+        if (category is null)
+        {
+            return NotFound();
+        }
+        
+        _context.Categories.Remove(category);
+        _context.SaveChanges();
+        TempData["success"] = "Category deleted successfully";
+        return RedirectToAction("Index");
     }
 }
